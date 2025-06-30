62149e21b76966fc2c202bcadb2f0fefdca97b2d
unknown <Mohamedadl52>
2025-06-30 14:04:28 +0200

    front edn upload2

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 // صفحات المستخدم
 import Home from '../views/HoMe.vue'
 import Starlink from '../views/StarlinkPage.vue'
-import Tagdem from '../views/TagdemElec.vue'
 import Login from '../views/loGin.vue'
 import Signup from '../views/signUp.vue'
 import Profile from '../views/proFile.vue'
 import Whous from '../views/HowUs.vue'
 import Contact from '../views/ContactUs.vue'
+import Tagdem from '../views/TagdemElec.vue'
 
 // صفحات الأدمن
 import Admin from '../views/admin/AdminPanel.vue'
