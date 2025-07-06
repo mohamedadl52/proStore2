af6a7e90dd0d1d48f3f5f2acf71d056202d654e6
unknown <Mohamedadl52>
2025-07-07 00:41:12 +0200

    test startlink2

--- src/views/proFile.vue
+++ src/views/proFile.vue
@@ -23,21 +23,11 @@
         </button>
       </div>
     </div>
-    <div class="mt-8 bg-white p-4 rounded shadow">
+    <div class="mt-8 bg-white  p-4 rounded shadow">
   <h3 class="text-xl font-semibold mb-4 text-center">الطلبات السابقة</h3>
   <div v-if="orders.length">
-    <!-- عرض كبطاقات على الهواتف -->
-<div class="space-y-4 sm:hidden">
-  <div v-for="(order, index) in starlinkOrders" :key="order._id" class="bg-white p-4 rounded shadow border">
-    <p><strong>الرقم:</strong> {{ index + 1 }}</p>
-    <p><strong>نوع المشكلة:</strong> {{ translateIssueType(order.issueType) }}</p>
-    <p><strong>التاريخ:</strong> {{ new Date(order.createdAt).toLocaleDateString() }}</p>
-    <p><strong>الحالة:</strong> {{ order.status }}</p>
-    <button @click="viewStarlinkOrder(order)" class="text-blue-600 hover:underline mt-2">عرض التفاصيل</button>
-  </div>
-</div>
-
-<table class="w-full text-right border hidden sm:table" dir="rtl">
+    
+<table class="w-full text-right px-6 border" dir="rtl">
   <thead class="bg-gray-100">
     <tr>
       <th class="border p-2">#</th>
@@ -87,9 +77,9 @@
 </div>
 
 <!-- starlink -->
-<h3 class="text-xl font-semibold mt-10 mb-4 text-center">طلبات Starlink</h3>
-<div v-if="starlinkOrders.length">
-  <table class="w-full text-right border" dir="rtl">
+<h3 class="text-xl   font-semibold mt-10 mb-4 text-center">طلبات Starlink</h3>
+<div v-if="starlinkOrders.length" class="px-8" >
+  <table class="w-full   text-right border" dir="rtl">
     <thead class="bg-gray-100">
       <tr>
         <th class="border p-2">#</th>
@@ -116,7 +106,7 @@
 
 
 <div style="direction: rtl;" v-if="showStarlinkModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-<div class="bg-white rounded-lg p-6 w-full max-w-md shadow-xl relative max-h-[90vh] overflow-y-auto">
+  <div class="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full relative">
     <h3 class="text-xl font-bold mb-4 text-center">تفاصيل طلب Starlink</h3>
 
     <div class="text-right space-y-2">
@@ -128,7 +118,7 @@
       <p><strong>تفاصيل:</strong> {{ selectedStarlink.details }}</p>
     </div>
     <div>
-<div class="mt-4 text-right overflow-x-auto flex space-x-4 rtl:space-x-reverse">
+      <div class="mt-4 space-y-4 text-right">
   <div v-if="selectedStarlink.identityImageUrl">
     <p class="font-semibold mb-1">صورة الهوية:</p>
     <img
