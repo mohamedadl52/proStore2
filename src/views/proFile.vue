8ffd81c5eab659cb7a51bb448c9ebc11d5719a5a
unknown <Mohamedadl52>
2025-07-08 20:16:52 +0200

    test

--- src/views/proFile.vue
+++ src/views/proFile.vue
@@ -262,13 +262,13 @@ onMounted(() => {
 });
 function getStarlinkStatusClass(status) {
   switch (status) {
-    case 'قيد المعالجة':
+    case 'قيد التنيذ ':
       return 'bg-yellow-100 text-yellow-800';
     case 'تم الحل':
       return 'bg-green-100 text-green-800';
     case 'مرفوض':
       return 'bg-red-100 text-red-800';
-    case 'مغلق':
+    case 'جاري الفحص':
       return 'bg-gray-200 text-gray-800';
     default:
       return 'bg-blue-100 text-blue-800'; // حالات أخرى
